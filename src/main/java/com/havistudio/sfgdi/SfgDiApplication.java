cc0f750a3b07a4c58ea679c50e98ae55ad95a48d
Chaviaras Konstantinos <chaviarask02@gmail.com>
2020-10-19 22:07:13 +0300

    update. 89. Multiple Property Files.

--- src/main/java/com/havistudio/sfgdi/SfgDiApplication.java
+++ src/main/java/com/havistudio/sfgdi/SfgDiApplication.java
@@ -5,6 +5,7 @@ import com.havistudio.sfgdi.controllers.GetterInjectedController;
 import com.havistudio.sfgdi.controllers.MyController;
 import com.havistudio.sfgdi.controllers.PropertyInjectedController;
 import com.havistudio.sfgdi.examplebeans.FakeDataSource;
+import com.havistudio.sfgdi.examplebeans.FakeJmsBroker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -20,6 +21,11 @@ public class SfgDiApplication {
 		FakeDataSource fakeDataSource = (FakeDataSource) ctx.getBean(FakeDataSource.class);
 
 		System.out.println(fakeDataSource.getUser());
+
+		FakeJmsBroker fakeJmsBroker = (FakeJmsBroker) ctx.getBean(FakeJmsBroker.class);
+
+		System.out.println(fakeJmsBroker.getUser());
+
 	}
 
 }
